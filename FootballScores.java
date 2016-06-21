cfe0c9557c45333cdef0dfcdbbab93123cb3c0c8
Madhav Kandukuri <madhav165@gmail.com>
2016-06-21 09:27:50 +0530

    Only show matches that already started

--- FootballScores.java
+++ FootballScores.java
@@ -13,14 +13,16 @@ public class FootballScores {
             String title = doc.title();
             Elements matches = doc.getElementsByClass("match");
             //MatchObject [] matches = new MatchObject[divs1.size()];
-            System.out.format("%-20s%-30s%-10s%-30s%5s", "Status", "Team 1", "vs", "Team2", "\n");
-            System.out.format("%-20s%-30s%-10s%-30s%5s", "======", "======", "==", "=====", "\n\n");
+            System.out.format("%-30s%-10s%-30s%5s", "Team 1", "vs", "Team2", "\n");
+            System.out.format("%-30s%-10s%-30s%5s", "======", "==", "=====", "\n\n");
             for (Element match : matches) {
                 Element status = match.getElementsByClass("status").first();
-                Element team1 = match.getElementsByClass("module-team").first();
-                Element team2 = match.getElementsByClass("module-team").last();
-                Element vs = match.getElementsByClass("vs").first();
-                System. out.format("%-20s%-30s%-10s%-20s%5s", status.text(),team1.text(),vs.text(),team2.text(),"\n\n");
+                if (status.text().equals("FT")) {
+                    Element team1 = match.getElementsByClass("module-team").first();
+                    Element team2 = match.getElementsByClass("module-team").last();
+                    Element vs = match.getElementsByClass("vs").first();
+                    System. out.format("%-30s%-10s%-20s%5s", team1.text(),vs.text(),team2.text(),"\n\n");
+                }
             }
         }
         catch (IOException e) {
